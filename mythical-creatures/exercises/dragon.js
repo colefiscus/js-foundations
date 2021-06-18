5106af6e1d4fa1d2da56f3a00f5baa4406f9b57c
colefiscus <coletfiscus@gmail.com>
2021-06-18 14:38:37 -0600

    Pass all dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.timesEaten = 0;
+  }
 
+  greet = (rider) => {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat = () => {
+    if (this.timesEaten < 2) {
+      this.timesEaten++
+      return
+    } else {
+      this.hungry = false
+    }
+  }
+}
+
+module.exports = Dragon;
