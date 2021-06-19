41a6e34e7a73e7299d28922999ee9d39bf0113d1
colefiscus <coletfiscus@gmail.com>
2021-06-18 18:23:50 -0600

    Pass all direwolf tests

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -3,6 +3,21 @@ class Direwolf {
     this.name = name;
     this.home = home || 'Beyond the Wall';
     this.size = size || 'Massive';
+    this.starksToProtect = [];
+    this.huntsWhiteWalkers = true;
+  }
+
+  protect(stark) {
+    if (this.home === stark.location && this.starksToProtect.length <= 1) {
+      this.starksToProtect.push(stark)
+      this.huntsWhiteWalkers = false;
+      stark.safe = true;
+    }
+  }
+
+  leave(stark) {
+    this.starksToProtect.splice(stark);
+    stark.safe = false;
   }
 }
 
--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,7 +1,25 @@
+const Direwolf = require("./direwolf");
+
 class Stark {
   constructor({ name, area }) {
     this.name = name;
     this.location = area || 'Winterfell';
+    this.safe = false;
+  }
+
+  sayHouseWords() {
+    if (!this.safe) {
+      return 'Winter is Coming'
+    } else {
+      return 'The North Remembers'
+    }
+  }
+
+  callDirewolf(name, home) {
+    const direwolf = new Direwolf('Nymeria', 'Winterfell')
+    direwolf.home = this.location;
+    direwolf.protect(this)
+    return direwolf;
   }
 }
 
